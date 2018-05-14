8f2a2ef35038fab551342d2b0e81eab1c5313956
Danielle Howard <edanielleh@gmail.com>
2018-05-14 17:07:23 -0400

    running app

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>I am editing this app</Text>
+        <Text>I got the project running with expo</Text>
+        <Text>Is this a POC?</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
