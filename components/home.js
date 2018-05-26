b00fdd74cf04a4d1b41f9e9963a44d08d5c3469f
Danielle Howard <edanielleh@gmail.com>
2018-05-25 20:38:28 -0400

    Buttons Added

--- components/home.js
+++ components/home.js
@@ -6,34 +6,36 @@ export default class Home extends Component {
   constructor() {
     super();
     this.state = {
-      val: "test"
+      val: false
     };
   }
-  handlePress() {
+  highlights() {
     this.setState({
-      val: "different"
+      val: true
     });
   };
   render() {
     return (
       <View >
-        <Text style={styles.title}>React and React Native</Text>
-        <Text style={styles.subtitle}> A Guide to Get Your First Application Up</Text>
-        <Text style={styles.title}>{ this.state.val }</Text>
-        <Button onPress={() => this.handlePress()}>Press Me!</Button>
+        <Text style={style.title}>React Native</Text>
+        <Text style={style.subtitle}>A Guide to Get Your First Application Up</Text>
+        <Button onPress={() => this.handlePress()}>Get Me Started</Button>
+        <Button onPress={() => this.highlights()}>Highlights</Button>
+        {this.state.val ? <Text style={style.gen}>Will redirect to new Component</Text> : <Text></Text>}
       </View>
     );
   }
 }
 
-const styles = StyleSheet.create({
+const style = StyleSheet.create({
   title: {
     color: 'blue',
     fontWeight: 'bold',
     fontSize: 35,
     textAlign: 'center',
     textAlignVertical: 'top',
-    marginTop: 50
+    marginTop: 50,
+    marginBottom: 10,
   },
   subtitle: {
     color: 'blue',
@@ -42,7 +44,7 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     textAlignVertical: 'top'
   },
-  red: {
-    color: 'red',
-  },
+  gen: {
+    textAlign: 'center',
+  }
 });
