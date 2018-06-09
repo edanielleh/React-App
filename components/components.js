cfe7423b2f34f92fb976522f15af4f9f9ea1a8fa
Danielle Howard <edanielleh@gmail.com>
2018-06-09 17:37:21 -0400

    Invalid Link Removed

--- components/components.js
+++ components/components.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { ScrollView, StyleSheet, Text, View,} from 'react-native';
+import { ScrollView, StyleSheet, Text, View, Linking} from 'react-native';
 import Button from 'react-native-button';
 import { Actions } from 'react-native-router-flux';
 import style from './css/components';
@@ -25,17 +25,15 @@ export default class Components extends Component {
         <Text style={style.title}>Components</Text>
         <View style={style.hairline} />
         <Text>Components allow us to split the UI into reusable, simple pieces. Typically the navbar and footer are commonly reused components.</Text>
-        <Text>To set up a component on our sample-app, install react-native-router-flux. Then require the packages Router  and Scene from it in App.js</Text>
-        <View>
+        <Text>To set up a component on our sample-app, install react-native-router-flux. Then require the packages Router and Scene from it in App.js</Text>
+
         <Icon name = "info" size = {50} onPress={() => this.info1()}/>
-        { this.state.info1 ? <View>
-        <Text>You can use various other libraries other than react-native-router-flux. </Text>
-        <Text style={{color: 'blue'}}
-      onPress={() => Linking.openURL('https://github.com/ReactTraining/react-router">React-Router')}>
-      React Router
-    </Text>
-    <Text>is another polular choice. Airbnb's native-navigation is still in beta. https://github.com/airbnb/native-navigation</Text> </View>: null }
+        { this.state.info1 ? 
+        <View>
+          <Text>You can use various other libraries other than react-native-router-flux. React Router
+          is another polular choice. </Text> 
         </View>
+        : null }
         </ScrollView>
         <Text style={style.footer}> created by Danielle Howard</Text>
       </View>
